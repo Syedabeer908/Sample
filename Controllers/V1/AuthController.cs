31087512a971d1df854d33c37f9148081824ea17
syed abeer <syed.abeer@qfnetwrok.org>
2026-05-07 05:23:28 -0700

    checking ci cd

--- Controllers/V1/AuthController.cs
+++ Controllers/V1/AuthController.cs
@@ -40,21 +40,21 @@ namespace WebApplication1.Controllers.V1
             return Ok(_resultHelper.Success<AuthResponseDto>(authResponse));
         }
 
-        //[HttpPost("refresh")]
-        //public async Task<IActionResult> Refresh(RefreshTokenDto dto)
-        //{
-        //    var authResponse = await _authService.Refresh(dto);
-        //    return Ok(_resultHelper.Success<AuthResponseDto>(authResponse));
-        //}
+        [HttpPost("refresh")]
+        public async Task<IActionResult> Refresh(RefreshTokenDto dto)
+        {
+            var authResponse = await _authService.Refresh(dto);
+            return Ok(_resultHelper.Success<AuthResponseDto>(authResponse));
+        }
 
-        //[HttpGet("whoami")]
-        //public IActionResult WhoAmI()
-        //{
-        //    return Ok(new
-        //    {
-        //        pod = Environment.MachineName,
-        //        time = DateTime.UtcNow
-        //    });
-        //}
+        [HttpGet("whoami")]
+        public IActionResult WhoAmI()
+        {
+            return Ok(new
+            {
+                pod = Environment.MachineName,
+                time = DateTime.UtcNow
+            });
+        }
     }
 }
